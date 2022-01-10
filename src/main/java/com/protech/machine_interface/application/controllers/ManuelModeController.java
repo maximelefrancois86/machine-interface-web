a26e056043d78b214bfa04f24e62c0c6700edac5
Aurélien JEANNEAU <aurelien.jeanneau@etu.emse.fr>
2022-01-10 01:06:05 +0100

    add routings

--- src/main/java/com/protech/machine_interface/application/controllers/ManuelModeController.java
+++ src/main/java/com/protech/machine_interface/application/controllers/ManuelModeController.java
@@ -1,12 +1,15 @@
 package com.protech.machine_interface.application.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ManuelModeController {
-    @GetMapping("/manuel")
-    public String ManuelModeController(){
+    @GetMapping("/machine/{id}/manuel")
+    public String ManuelModeController(@PathVariable("id") String id, Model model){
+        model.addAttribute("id", id);
         return "ManuelMode";
     }
 }
